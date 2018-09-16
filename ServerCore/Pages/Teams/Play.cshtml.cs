6993e4c678919ab20e2a7deb1c821c47de07c8a4
Kenny Young <kennyy@microsoft.com>
2018-09-15 19:02:40 -0700

    oops, ment to delete stray usings

--- ServerCore/Pages/Teams/Play.cshtml.cs
+++ ServerCore/Pages/Teams/Play.cshtml.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ServerCore.DataModel;
 using ServerCore.ModelBases;
-using ServerCore.Models;
 
 namespace ServerCore.Pages.Teams
 {
