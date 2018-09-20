65b1f467429aa36c71858eec3d9edd74bc8c2fea
Kenny Young <kennyy@microsoft.com>
2018-09-19 22:28:30 -0700

    Ensure PuzzleStatePerTeam on Write Only
    Trying lots of things to get better perf out of this table without
    sacrificing correctness.
    
    Several changes here:
    
    1. I've removed the ID property of the PuzzleStatePerTeam table and made
    the PuzzleID+TeamID into the key.
    
    2. I've moved all state access into PuzzleStateHelper so it can be
    shared across pages.
    
    3. I've made a readonly accessor that is resilient to missing states. It
    performs joins - but, since the joins make untracked results, they can't
    be used for edits.
    
    4. I've made a writable accessor that ensures that states exist. It is
    possible that some extremely rare timing issue will cause the write to
    fail, but it will not break the event longterm.
    
    If someone can write these better, they are more than welcome to do so,
    but this is the best I can figure out and I've gone down several blind
    alleys just to get here.

---
+++ Data/Migrations/20180918162310_PuzzleStatePerTeamKey.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class PuzzleStatePerTeamKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PuzzleStatePerTeam_Puzzles_PuzzleID",
+                table: "PuzzleStatePerTeam");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PuzzleStatePerTeam_Teams_TeamID",
+                table: "PuzzleStatePerTeam");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PuzzleStatePerTeam",
+                table: "PuzzleStatePerTeam");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PuzzleStatePerTeam_PuzzleID",
+                table: "PuzzleStatePerTeam");
+
+            migrationBuilder.DropColumn(
+                name: "ID",
+                table: "PuzzleStatePerTeam");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TeamID",
+                table: "PuzzleStatePerTeam",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PuzzleID",
+                table: "PuzzleStatePerTeam",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PuzzleStatePerTeam",
+                table: "PuzzleStatePerTeam",
+                columns: new[] { "PuzzleID", "TeamID" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PuzzleStatePerTeam_Puzzles_PuzzleID",
+                table: "PuzzleStatePerTeam",
+                column: "PuzzleID",
+                principalTable: "Puzzles",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PuzzleStatePerTeam_Teams_TeamID",
+                table: "PuzzleStatePerTeam",
+                column: "TeamID",
+                principalTable: "Teams",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PuzzleStatePerTeam_Puzzles_PuzzleID",
+                table: "PuzzleStatePerTeam");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PuzzleStatePerTeam_Teams_TeamID",
+                table: "PuzzleStatePerTeam");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PuzzleStatePerTeam",
+                table: "PuzzleStatePerTeam");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TeamID",
+                table: "PuzzleStatePerTeam",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PuzzleID",
+                table: "PuzzleStatePerTeam",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddColumn<int>(
+                name: "ID",
+                table: "PuzzleStatePerTeam",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PuzzleStatePerTeam",
+                table: "PuzzleStatePerTeam",
+                column: "ID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PuzzleStatePerTeam_PuzzleID",
+                table: "PuzzleStatePerTeam",
+                column: "PuzzleID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PuzzleStatePerTeam_Puzzles_PuzzleID",
+                table: "PuzzleStatePerTeam",
+                column: "PuzzleID",
+                principalTable: "Puzzles",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PuzzleStatePerTeam_Teams_TeamID",
+                table: "PuzzleStatePerTeam",
+                column: "TeamID",
+                principalTable: "Teams",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ ServerCore/PuzzleStateHelper.cs
@@ -0,0 +1,162 @@
+﻿using ServerCore.DataModel;
+using ServerCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ServerCore
+{
+    public class PuzzleStateHelper
+    {
+        /// <summary>
+        /// Get a read-only query of puzzle state. You won't be able to write to these values, but the query will be resilient to state records that are missing on the server.
+        /// </summary>
+        /// <param name="context">The puzzle DB context</param>
+        /// <param name="eventObj">The event we are querying from</param>
+        /// <param name="puzzle">The puzzle; if null, get all puzzles in the event.</param>
+        /// <param name="team">The team; if null, get all the teams in the event.</param>
+        /// <returns>A query of PuzzleStatePerTeam objects that can be sorted and instantiated, but you can't edit the results.</returns>
+        public static IQueryable<PuzzleStatePerTeam> GetReadOnlyQuery(PuzzleServerContext context, Event eventObj, Puzzle puzzle, Team team)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("Context required.");
+            }
+
+            if (eventObj == null)
+            {
+                throw new ArgumentNullException("Event required.");
+            }
+
+            if (puzzle != null && team != null)
+            {
+                return context.PuzzleStatePerTeam
+                    .Where(state => state.Puzzle == puzzle && state.Team == team)
+                    .DefaultIfEmpty(new DataModel.PuzzleStatePerTeam()
+                    {
+                        Puzzle = puzzle,
+                        PuzzleID = puzzle.ID,
+                        Team = team,
+                        TeamID = team.ID
+                    });
+            }
+
+            if (puzzle != null)
+            {
+                var teams = context.Teams.Where(t => t.Event == eventObj);
+                var states = context.PuzzleStatePerTeam.Where(state => state.Puzzle == puzzle);
+
+                return from t in teams
+                       join state in states on t.ID equals state.TeamID into tmp
+                       from teamstate in tmp.DefaultIfEmpty()
+                       select new PuzzleStatePerTeam
+                       {
+                           Puzzle = puzzle,
+                           PuzzleID = puzzle.ID,
+                           Team = t,
+                           TeamID = t.ID,
+                           UnlockedTime = teamstate == null ? null : teamstate.UnlockedTime,
+                           SolvedTime = teamstate == null ? null : teamstate.SolvedTime,
+                           Printed = teamstate == null ? false : teamstate.Printed,
+                           Notes = teamstate == null ? null : teamstate.Notes
+                       };
+            }
+
+            if (team != null)
+            {
+                var puzzles = context.Puzzles.Where(p => p.Event == eventObj);
+                var states = context.PuzzleStatePerTeam.Where(state => state.Team == team);
+
+                return from p in puzzles
+                       join state in states on p.ID equals state.PuzzleID into tmp
+                       from teamstate in tmp.DefaultIfEmpty()
+                       select new PuzzleStatePerTeam
+                       {
+                           Puzzle = p,
+                           PuzzleID = p.ID,
+                           Team = team,
+                           TeamID = team.ID,
+                           UnlockedTime = teamstate == null ? null : teamstate.UnlockedTime,
+                           SolvedTime = teamstate == null ? null : teamstate.SolvedTime,
+                           Printed = teamstate == null ? false : teamstate.Printed,
+                           Notes = teamstate == null ? null : teamstate.Notes
+                       };
+            }
+
+            throw new NotImplementedException("Full event query is NYI but needed for puzzle state map");
+        }
+
+        /// <summary>
+        /// Get a writable query of puzzle state. In the course of constructing the query, it will instantiate any state records that are missing on the server.
+        /// </summary>
+        /// <param name="context">The puzzle DB context</param>
+        /// <param name="eventObj">The event we are querying from</param>
+        /// <param name="puzzle">The puzzle; if null, get all puzzles in the event.</param>
+        /// <param name="team">The team; if null, get all the teams in the event.</param>
+        /// <returns>A query of PuzzleStatePerTeam objects that can be sorted and instantiated, but you can't edit the results.</returns>
+        public static async Task<IQueryable<PuzzleStatePerTeam>> GetReadWriteQueryAsync(PuzzleServerContext context, Event eventObj, Puzzle puzzle, Team team)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("Context required.");
+            }
+
+            if (eventObj == null)
+            {
+                throw new ArgumentNullException("Event required.");
+            }
+
+            if (puzzle != null && team != null)
+            {
+                PuzzleStatePerTeam state = await context.PuzzleStatePerTeam.Where(s => s.Puzzle == puzzle && s.Team == team).FirstOrDefaultAsync();
+                if (state == null)
+                {
+                    context.PuzzleStatePerTeam.Add(new DataModel.PuzzleStatePerTeam() { Puzzle = puzzle, Team = team });
+                    await context.SaveChangesAsync(); // query below will not return this unless we save
+                }
+
+                return context.PuzzleStatePerTeam.Where(s => s.Puzzle == puzzle && s.Team == team);
+            }
+
+            if (puzzle != null)
+            {
+                var teamIdsQ = context.Teams.Where(p => p.Event == eventObj).Select(p => p.ID);
+                var puzzleStateTeamIdsQ = context.PuzzleStatePerTeam.Where(s => s.Puzzle == puzzle).Select(s => s.TeamID);
+                var teamIdsWithoutState = await teamIdsQ.Except(puzzleStateTeamIdsQ).ToListAsync();
+
+                if (teamIdsWithoutState.Count > 0)
+                {
+                    for (int i = 0; i < teamIdsWithoutState.Count; i++)
+                    {
+                        context.PuzzleStatePerTeam.Add(new DataModel.PuzzleStatePerTeam() { Puzzle = puzzle, TeamID = teamIdsWithoutState[i] });
+                    }
+                    await context.SaveChangesAsync(); // query below will not return these unless we save
+                }
+
+                return context.PuzzleStatePerTeam.Where(state => state.Puzzle == puzzle);
+            }
+
+            if (team != null)
+            {
+                var puzzleIdsQ = context.Puzzles.Where(p => p.Event == eventObj).Select(p => p.ID);
+                var puzzleStatePuzzleIdsQ = context.PuzzleStatePerTeam.Where(s => s.Team == team).Select(s => s.PuzzleID);
+                var puzzleIdsWithoutState = await puzzleIdsQ.Except(puzzleStatePuzzleIdsQ).ToListAsync();
+
+                if (puzzleIdsWithoutState.Count > 0)
+                {
+                    for (int i = 0; i < puzzleIdsWithoutState.Count; i++)
+                    {
+                        context.PuzzleStatePerTeam.Add(new DataModel.PuzzleStatePerTeam() { Team = team, PuzzleID = puzzleIdsWithoutState[i] });
+                    }
+                    await context.SaveChangesAsync(); // query below will not return these unless we save
+                }
+
+                return context.PuzzleStatePerTeam.Where(state => state.Team == team);
+            }
+
+            throw new NotImplementedException("Full event query is NYI and may never be needed");
+        }
+    }
+}
