9865bdf6eaea08c8c01d22f59b539892c73b2754
Lambert Wang <law@microsoft.com>
2018-09-26 20:10:19 -0700

    Added migration

---
+++ Data/Migrations/20180927030906_AddEventContactEmail.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class AddEventContactEmail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContactEmail",
+                table: "Events",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContactEmail",
+                table: "Events");
+        }
+    }
+}
