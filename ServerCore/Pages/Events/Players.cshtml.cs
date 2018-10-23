01e0a00b68c6971e5dd7085b0614272090038c2d
Jenna Netland <jenetlan@microsoft.com>
2018-10-22 19:38:41 -0700

    Can now view players in each event

---
+++ ServerCore/Pages/Events/Players.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ServerCore.DataModel;
+using ServerCore.ModelBases;
+
+namespace ServerCore.Pages.Events
+{
+    public class PlayersModel : EventSpecificPageModel
+    {
+        private readonly ServerCore.DataModel.PuzzleServerContext _context;
+
+        public IList<TeamMembers> Players { get; set; }
+
+        public PlayersModel(ServerCore.DataModel.PuzzleServerContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int teamId)
+        {
+            Players = await _context.TeamMembers
+                .Where(member => member.Team.Event == Event)
+                .ToListAsync();
+
+            return Page();
+        }
+    }
+}
