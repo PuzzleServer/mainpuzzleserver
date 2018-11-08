66d04cb3fc19dfa439d160422ca34164a0df881e
Megan Quinn <asyasky@gmail.com>
2018-11-07 20:10:02 -0800

    Add IsAdmin, IsAuthor, and GetPuzzles methods

--- Data/DataModel/PuzzleUser.cs
+++ Data/DataModel/PuzzleUser.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using Microsoft.AspNetCore.Identity;
@@ -31,6 +32,21 @@ namespace ServerCore.DataModel
         public static PuzzleUser GetPuzzleUser(string identityUserId, PuzzleServerContext dbContext)
         {
             return dbContext.PuzzleUsers.Where(user => user.IdentityUserId == identityUserId).FirstOrDefault();
+        }
+
+        public bool IsAuthorForEvent(Event thisEvent, PuzzleServerContext puzzleServerContext)
+        {
+            return puzzleServerContext.EventAuthors.Where(a => a.Author.ID == ID && a.Event.ID == thisEvent.ID).Any();
+        }
+
+        public List<Puzzle> GetPuzzlesForAuthorAndEvent(PuzzleServerContext dbContext, Event thisEvent)
+        {
+            return dbContext.PuzzleAuthors.Where(pa => pa.Author.ID == ID && pa.Puzzle.Event.ID == thisEvent.ID).Select(pa => pa.Puzzle).ToList();
+        }
+
+        public bool IsAdminForEvent (PuzzleServerContext dbContext, Event thisEvent)
+        {
+            return dbContext.EventAdmins.Where(a => a.Admin.ID == ID && a.Event.ID == thisEvent.ID).Any();
         }
     }
 }
