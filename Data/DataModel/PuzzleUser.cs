f74e94a5a0ad8ead2edf87d9691cd6b29a503905
Megan Quinn <asyasky@gmail.com>
2018-12-01 16:32:37 -0800

    Move GetPuzzlesFor Author out of data model.

--- Data/DataModel/PuzzleUser.cs
+++ Data/DataModel/PuzzleUser.cs
@@ -45,16 +45,6 @@ namespace ServerCore.DataModel
         }
 
         /// <summary>
-        /// Returns a list of the author's puzzles for the given event
-        /// </summary>
-        /// <param name="thisEvent">The event that's being checked</param>
-        /// <param name="puzzleServerContext">Current PuzzleServerContext</param>
-        public List<Puzzle> GetPuzzlesForAuthorAndEvent(PuzzleServerContext dbContext, Event thisEvent)
-        {
-            return dbContext.PuzzleAuthors.Where(pa => pa.Author.ID == ID && pa.Puzzle.Event.ID == thisEvent.ID).Select(pa => pa.Puzzle).ToList();
-        }
-
-        /// <summary>
         /// Returns whether or not a user is an admin for the given event
         /// </summary>
         /// <param name="thisEvent">The event that's being checked</param>
---
+++ ServerCore/AuthorDashboardHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ServerCore.DataModel;
+
+namespace ServerCore
+{
+    public class AuthorDashboardHelper
+    {
+        /// <summary>
+        /// Returns a list of the author's puzzles for the given event
+        /// </summary>
+        /// <param name="thisEvent">The event that's being checked</param>
+        /// <param name="puzzleServerContext">Current PuzzleServerContext</param>
+        public List<Puzzle> GetPuzzlesForAuthorAndEvent(PuzzleServerContext dbContext, PuzzleUser user, Event thisEvent)
+        {
+            return dbContext.PuzzleAuthors.Where(pa => pa.Author.ID == user.ID && pa.Puzzle.Event.ID == thisEvent.ID).Select(pa => pa.Puzzle).ToList();
+        }
+    }
+}
