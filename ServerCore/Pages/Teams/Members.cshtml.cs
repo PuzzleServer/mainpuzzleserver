9224987dda6509e859801ce5dff0b80cef8e45d6
Jenna Netland <jenetlan@microsoft.com>
2018-12-19 19:00:55 -0800

    Debugging player user - committing before updating to latest

--- ServerCore/Pages/Teams/Members.cshtml.cs
+++ ServerCore/Pages/Teams/Members.cshtml.cs
@@ -36,8 +36,7 @@ namespace ServerCore.Pages.Teams
             StringBuilder emailList = new StringBuilder("");
             foreach (TeamMembers Member in Members)
             {
-                emailList.Append(Member.Member.ID);//ToString());
-                //TODO - figure out why this isn't working: emailList.Append(Member.Member.Email + "; ");
+                emailList.Append(Member.Member.Email + "; ");
             }
             Emails = emailList.ToString();
 
