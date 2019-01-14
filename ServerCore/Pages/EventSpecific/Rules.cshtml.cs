53b5460684c4dd13d94c1a360ec79635c424f72b
Jenna Netland <jenetlan@microsoft.com>
2019-01-13 16:31:19 -0800

    Linked Event & Resource navigation buttons to the correct pages

---
+++ ServerCore/Pages/EventSpecific/Rules.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ServerCore.DataModel;
+using ServerCore.ModelBases;
+
+namespace ServerCore.Pages.EventSpecific
+{
+    public class RulesModel : EventSpecificPageModel
+    {
+        public RulesModel(PuzzleServerContext serverContext, UserManager<IdentityUser> userManager): base(serverContext, userManager)
+        {
+
+        }
+    }
+}
