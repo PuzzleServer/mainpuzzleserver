8e3c13164603ccc762e8971d05eaa5817d563384
Kenny Young <kennyy@microsoft.com>
2019-01-17 15:38:37 -0800

    Add sorting

--- ServerCore/Pages/Submissions/AuthorIndex.cshtml.cs
+++ ServerCore/Pages/Submissions/AuthorIndex.cshtml.cs
@@ -18,14 +18,20 @@ namespace ServerCore.Pages.Submissions
         {
         }
 
-        public IList<Submission> Submissions { get; set; }
+        public List<Submission> Submissions { get; set; }
 
         public Puzzle Puzzle { get; set; }
 
         public Team  Team { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(int? puzzleId, int? teamId)
+        public SortOrder? Sort { get; set; }
+
+        public const SortOrder DefaultSort = SortOrder.TimeDescending;
+
+        public async Task<IActionResult> OnGetAsync(int? puzzleId, int? teamId, SortOrder? sort)
         {
+            Sort = sort;
+
             if (puzzleId == null)
             {
                 if (EventRole == EventRole.admin)
@@ -89,7 +95,80 @@ namespace ServerCore.Pages.Submissions
                 Team = await _context.Teams.Where(m => m.ID == teamId).FirstOrDefaultAsync();
             }
 
+            switch (sort ?? DefaultSort)
+            {
+                case SortOrder.PlayerAscending:
+                    Submissions.Sort((a, b) => a.Submitter.Name.CompareTo(b.Submitter.Name));
+                    break;
+                case SortOrder.PlayerDescending:
+                    Submissions.Sort((a, b) => -a.Submitter.Name.CompareTo(b.Submitter.Name));
+                    break;
+                case SortOrder.TeamAscending:
+                    Submissions.Sort((a, b) => a.Team.Name.CompareTo(b.Team.Name));
+                    break;
+                case SortOrder.TeamDescending:
+                    Submissions.Sort((a, b) => -a.Team.Name.CompareTo(b.Team.Name));
+                    break;
+                case SortOrder.PuzzleAscending:
+                    Submissions.Sort((a, b) => a.Puzzle.Name.CompareTo(b.Puzzle.Name));
+                    break;
+                case SortOrder.PuzzleDescending:
+                    Submissions.Sort((a, b) => -a.Puzzle.Name.CompareTo(b.Puzzle.Name));
+                    break;
+                case SortOrder.ResponseAscending:
+                    Submissions.Sort((a, b) => a.Response.ResponseText.CompareTo(b.Response.ResponseText));
+                    break;
+                case SortOrder.ResponseDescending:
+                    Submissions.Sort((a, b) => -a.Response.ResponseText.CompareTo(b.Response.ResponseText));
+                    break;
+                case SortOrder.SubmissionAscending:
+                    Submissions.Sort((a, b) => a.SubmissionText.CompareTo(b.SubmissionText));
+                    break;
+                case SortOrder.SubmissionDescending:
+                    Submissions.Sort((a, b) => -a.SubmissionText.CompareTo(b.SubmissionText));
+                    break;
+                case SortOrder.TimeAscending:
+                    Submissions.Sort((a, b) => a.TimeSubmitted.CompareTo(b.TimeSubmitted));
+                    break;
+                case SortOrder.TimeDescending:
+                    Submissions.Sort((a, b) => -a.TimeSubmitted.CompareTo(b.TimeSubmitted));
+                    break;
+            }
+
             return Page();
+        }
+
+        public SortOrder? SortForColumnLink(SortOrder ascendingSort, SortOrder descendingSort)
+        {
+            SortOrder result = ascendingSort;
+
+            if (result == (Sort ?? DefaultSort))
+            {
+                result = descendingSort;
+            }
+
+            if (result == DefaultSort)
+            {
+                return null;
+            }
+
+            return result;
+        }
+
+        public enum SortOrder
+        {
+            PlayerAscending,
+            PlayerDescending,
+            TeamAscending,
+            TeamDescending,
+            PuzzleAscending,
+            PuzzleDescending,
+            ResponseAscending,
+            ResponseDescending,
+            SubmissionAscending,
+            SubmissionDescending,
+            TimeAscending,
+            TimeDescending
         }
     }
 }
