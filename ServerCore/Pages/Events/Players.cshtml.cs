463dc2a6d950334349c85e8392e631010f497dd6
Jenna Netland <jenetlan@microsoft.com>
2019-01-21 11:22:55 -0800

    Added admin & author lists to the Users (Players) page

--- ServerCore/Pages/Events/Players.cshtml.cs
+++ ServerCore/Pages/Events/Players.cshtml.cs
@@ -12,26 +12,64 @@ namespace ServerCore.Pages.Events
 {
     public class PlayersModel : EventSpecificPageModel
     {
+        public IList<PuzzleUser> Admins { get; set; }
+
+        public string AdminEmails { get; set; }
+
+        public IList<PuzzleUser> Authors { get; set; }
+
+        public string AuthorEmails { get; set; }
+
         public IList<TeamMembers> Players { get; set; }
 
-        public string Emails { get; set; }
+        public string PlayerEmails { get; set; }
 
         public PlayersModel(PuzzleServerContext serverContext, UserManager<IdentityUser> userManager) : base(serverContext, userManager)
         {
         }
 
-        public async Task<IActionResult> OnGetAsync(int teamId)
+        public async Task<IActionResult> OnGetAsync()
         {
+            // Get admins
+
+            Admins = await _context.EventAdmins
+                .Where(admin => admin.Event == Event).Select(admin => admin.Admin)
+                .ToListAsync();
+
+            StringBuilder adminEmailList = new StringBuilder("");
+            foreach (PuzzleUser admin in Admins)
+            {
+                adminEmailList.Append(admin.Email + "; ");
+            }
+            AdminEmails = adminEmailList.ToString();
+
+            // Get authors
+
+            Authors = await _context.EventAuthors
+                .Where(author => author.Event == Event).Select(author => author.Author)
+                .ToListAsync();
+
+            StringBuilder authorEmailList = new StringBuilder("");
+            foreach (PuzzleUser author in Authors)
+            {
+                authorEmailList.Append(author.Email + "; ");
+            }
+            AuthorEmails = authorEmailList.ToString();
+
+            // Get players
+
             Players = await _context.TeamMembers
                 .Where(member => member.Team.Event == Event)
                 .ToListAsync();
 
-            StringBuilder emailList = new StringBuilder("");
+            StringBuilder playerEmailList = new StringBuilder("");
             foreach (TeamMembers Player in Players)
             {
-                emailList.Append(Player.Member.Email + "; ");
+                playerEmailList.Append(Player.Member.Email + "; ");
             }
-            Emails = emailList.ToString();
+            PlayerEmails = playerEmailList.ToString();
+
+            // Return page
 
             return Page();
         }
