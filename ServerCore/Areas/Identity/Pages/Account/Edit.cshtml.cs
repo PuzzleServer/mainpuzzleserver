998eb49fab566e6d8bafacc3ec3b6aa24a19106c
Kenny Young <42425095+tabascq@users.noreply.github.com>
2019-02-19 19:31:18 -0800

    Edit Player Properties (#184) (#240)
    * Edit Player Properties (#184)
    
    Updated the profile page for the logged in user to update all their properties.
    
    Also added a page that can be accessed from the team members list or the admin users list, which allows editing if the user is the current user or a global admin.
    
    Along the way, fixed a bug where editing could clear the global admin flag.
    
    * CR changes
    
    * Better security on user page

---
+++ ServerCore/Areas/Identity/Pages/Account/Edit.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ServerCore.DataModel;
+
+namespace ServerCore.Areas.Identity.Pages.Account
+{
+    [Authorize(Policy = "IsGlobalAdmin")]
+    public class EditModel : PageModel
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly PuzzleServerContext _context;
+
+        public EditModel(
+            UserManager<IdentityUser> userManager,
+            PuzzleServerContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        [BindProperty]
+        public PuzzleUser PuzzleUser { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int userId, string returnUrl = null)
+        {
+            PuzzleUser = await _context.PuzzleUsers.Where((p) => p.ID == userId).FirstOrDefaultAsync();
+            ReturnUrl = returnUrl;
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var thisPuzzleUser = await PuzzleUser.GetPuzzleUserForCurrentUser(_context, User, _userManager);
+
+            if (thisPuzzleUser != null)
+            {
+                _context.Entry(thisPuzzleUser).State = EntityState.Detached;
+            }
+
+            _context.Attach(PuzzleUser).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+            return Redirect(returnUrl);
+        }
+    }
+}
