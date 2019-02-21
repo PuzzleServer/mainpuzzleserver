c1a49205a6c3e943824fd7221516bb42f67bd3f9
Kenny Young <42425095+tabascq@users.noreply.github.com>
2019-02-20 22:08:39 -0800

    Fix #188 (#246)
    * Fix #188
    
    Unsure how this code ever worked at all. Maybe it only worked in the empty DB case. Regardless, it works now.
    
    * Better security

--- ServerCore/Pages/Puzzles/Edit.cshtml.cs
+++ ServerCore/Pages/Puzzles/Edit.cshtml.cs
@@ -92,7 +92,7 @@ namespace ServerCore.Pages.Puzzles
             PotentialPrerequisitesOf = await potentialPrerequitesOfQ.OrderBy(p => p.Name).ToListAsync();
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(int puzzleId)
         {
             ModelState.Remove("Puzzle.Event");
             if (!ModelState.IsValid)
@@ -100,6 +100,8 @@ namespace ServerCore.Pages.Puzzles
                 await PopulateUIAsync();
                 return Page();
             }
+
+            Puzzle.ID = puzzleId; // to be safe
 
             _context.Attach(Puzzle).State = EntityState.Modified;
 
@@ -122,48 +124,48 @@ namespace ServerCore.Pages.Puzzles
             return RedirectToPage("./Index");
         }
 
-        public async Task<IActionResult> OnPostAddAuthorAsync()
+        public async Task<IActionResult> OnPostAddAuthorAsync(int puzzleId)
         {
-            if (!(await _context.EventAuthors.Select(m => m.Author.ID == NewAuthorID && m.Event == Event).AnyAsync()))
+            if (!(await _context.EventAuthors.Where(m => m.Author.ID == NewAuthorID && m.Event == Event).AnyAsync()))
             {
                 return NotFound();
             }
 
-            if (!(await _context.PuzzleAuthors.Select(m => m.PuzzleID == Puzzle.ID && m.AuthorID == NewAuthorID).AnyAsync()))
+            if (!(await _context.PuzzleAuthors.Where(m => m.PuzzleID == puzzleId && m.AuthorID == NewAuthorID).AnyAsync()))
             {
-                _context.PuzzleAuthors.Add(new PuzzleAuthors() { PuzzleID = Puzzle.ID, AuthorID = NewAuthorID });
+                _context.PuzzleAuthors.Add(new PuzzleAuthors() { PuzzleID = puzzleId, AuthorID = NewAuthorID });
                 await _context.SaveChangesAsync();
             }
 
             return RedirectToPage();
         }
 
-        public async Task<IActionResult> OnPostAddPrerequisiteAsync()
+        public async Task<IActionResult> OnPostAddPrerequisiteAsync(int puzzleId)
         {
             if (!PuzzleExists(NewPrerequisiteID))
             {
                 return NotFound();
             }
 
-            if (!PrerequisiteExists(Puzzle.ID, NewPrerequisiteID))
+            if (!PrerequisiteExists(puzzleId, NewPrerequisiteID))
             {
-                _context.Prerequisites.Add(new Prerequisites() { PuzzleID = Puzzle.ID, PrerequisiteID = NewPrerequisiteID });
+                _context.Prerequisites.Add(new Prerequisites() { PuzzleID = puzzleId, PrerequisiteID = NewPrerequisiteID });
                 await _context.SaveChangesAsync();
             }
 
             return RedirectToPage();
         }
 
-        public async Task<IActionResult> OnPostAddPrerequisiteOfAsync()
+        public async Task<IActionResult> OnPostAddPrerequisiteOfAsync(int puzzleId)
         {
             if (!PuzzleExists(NewPrerequisiteOfID))
             {
                 return NotFound();
             }
 
-            if (!PrerequisiteExists(NewPrerequisiteOfID, Puzzle.ID))
+            if (!PrerequisiteExists(NewPrerequisiteOfID, puzzleId))
             {
-                _context.Prerequisites.Add(new Prerequisites() { PuzzleID = NewPrerequisiteOfID, PrerequisiteID = Puzzle.ID });
+                _context.Prerequisites.Add(new Prerequisites() { PuzzleID = NewPrerequisiteOfID, PrerequisiteID = puzzleId });
                 await _context.SaveChangesAsync();
             }
 
