66208b6da2843821678b176181192f370a66eaa2
Morgan Brown <atrader42@hotmail.com>
2019-02-25 19:29:03 -0800

    Add empty puzzle picker page

---
+++ ServerCore/Pages/EventSpecific/PH20/PuzzlePicker.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ServerCore.DataModel;
+using ServerCore.ModelBases;
+
+namespace ServerCore.Pages.EventSpecific.PH20
+{
+    public class PuzzlePickerModel : EventSpecificPageModel
+    {
+        public PuzzlePickerModel(PuzzleServerContext serverContext, UserManager<IdentityUser> manager) : base(serverContext, manager)
+        {
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
