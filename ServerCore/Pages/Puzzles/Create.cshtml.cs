62af4e68c311a6e0e69d50482c24e35fd479e243
Kenny Young <kennyy@microsoft.com>
2019-03-06 21:12:11 -0800

    Faster Puzzle Entry
    Streamlined the Create page to only a few fields and a selection of puzzle types that fills out defaults for many others.
    
    Also streamlined the Edit page to separate properties into basic and advanced buckets, and shorten the overall height so that the Prerequisites section is visible.

--- ServerCore/Pages/Puzzles/Create.cshtml.cs
+++ ServerCore/Pages/Puzzles/Create.cshtml.cs
@@ -11,7 +11,7 @@ namespace ServerCore.Pages.Puzzles
     public class CreateModel : EventSpecificPageModel
     {
         [BindProperty]
-        public Puzzle Puzzle { get; set; }
+        public MiniPuzzle Puzzle { get; set; }
 
         public CreateModel(PuzzleServerContext serverContext, UserManager<IdentityUser> userManager) : base(serverContext, userManager)
         {
@@ -20,31 +20,82 @@ namespace ServerCore.Pages.Puzzles
         public IActionResult OnGet()
         {
             // Populate default fields
-            Puzzle = new Puzzle();
-            Puzzle.IsPuzzle = true;
-            Puzzle.SolveValue = 10;
-            Puzzle.OrderInGroup = 0;
-            Puzzle.MinPrerequisiteCount = 0;
+            Puzzle = new MiniPuzzle();
 
             return Page();
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
-            ModelState.Remove("Puzzle.Event");
             if (!ModelState.IsValid)
             {
                 return Page();
             }
 
-            Puzzle.Event = Event;
+            Puzzle p = new Puzzle();
 
-            _context.Puzzles.Add(Puzzle);
-            _context.PuzzleAuthors.Add(new PuzzleAuthors() { Puzzle = Puzzle, Author = LoggedInUser });
+            p.Event = Event;
+            p.Name = Puzzle.Name;
+            p.Group = Puzzle.Group;
+            p.OrderInGroup = Puzzle.OrderInGroup;
+
+            switch (Puzzle.Type)
+            {
+                case PuzzleType.FinalMetaPuzzle:
+                    p.IsFinalPuzzle = true;
+                    p.IsMetaPuzzle = true;
+                    p.IsPuzzle = true;
+                    p.SolveValue = 1000;
+                    p.MinPrerequisiteCount = 1;
+                    break;
+                case PuzzleType.MetaPuzzle:
+                    p.IsMetaPuzzle = true;
+                    p.IsPuzzle = true;
+                    p.SolveValue = 50;
+                    p.MinPrerequisiteCount = 1;
+                    break;
+                case PuzzleType.CheatCode:
+                    p.IsPuzzle = true;
+                    p.IsCheatCode = true;
+                    p.SolveValue = -1;
+                    p.MinPrerequisiteCount = 1;
+                    break;
+                case PuzzleType.NormalPuzzle:
+                    p.IsPuzzle = true;
+                    p.SolveValue = 10;
+                    p.MinPrerequisiteCount = 1;
+                    break;
+                case PuzzleType.InvisiblePuzzle:
+                    p.MinPrerequisiteCount = 1;
+                    break;
+                case PuzzleType.StartPuzzle:
+                    break;
+            }
+
+            _context.Puzzles.Add(p);
+            _context.PuzzleAuthors.Add(new PuzzleAuthors() { Puzzle = p, Author = LoggedInUser });
 
             await _context.SaveChangesAsync();
 
-            return RedirectToPage("./Index");
+            return RedirectToPage("./Edit", new { puzzleId = p.ID });
+        }
+
+        public class MiniPuzzle
+        {
+            public string Name { get; set; }
+            public string Group { get; set; }
+            public int OrderInGroup { get; set; }
+            public PuzzleType Type { get; set; }
+        }
+
+        public enum PuzzleType
+        {
+            NormalPuzzle,
+            MetaPuzzle,
+            FinalMetaPuzzle,
+            CheatCode,
+            StartPuzzle,
+            InvisiblePuzzle
         }
     }
 }
