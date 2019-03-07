0e8b40b6d8cc41cdb8e466efc05b0d25a5b85123
Clarice Costello <claricostello@outlook.com>
2019-03-07 12:48:36 -0800

    Revert "Wrote CanCreateEvent test"
    This reverts commit 9bfa05a2108819fd198df61ac0c5e1c65336da39.

--- ServerCore.Test/UnitTest1.cs
+++ ServerCore.Test/UnitTest1.cs
@@ -1,6 +1,4 @@
-using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ServerCore.DataModel;
 
 namespace ServerCore.Test
 {
@@ -8,16 +6,8 @@ namespace ServerCore.Test
     public class UnitTest1
     {
         [TestMethod]
-        public void CanCreateEvent()
+        public void TestMethod1()
         {
-            DbContextOptions<PuzzleServerContext> options = new DbContextOptionsBuilder<PuzzleServerContext>().UseInMemoryDatabase("TestChanges").Options;
-            using (PuzzleServerContext context = new PuzzleServerContext(options))
-            {
-                Event Event = new Event();
-                Event.Name = "Check";
-                context.Events.Add(Event);
-                Assert.AreEqual(1, Event.ID);
-            }
         }
     }
 }
