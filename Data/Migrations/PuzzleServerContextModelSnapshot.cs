493479fedf48eb40e5a9d5e2e328e18b21f1c15a
Kenny Young <42425095+tabascq@users.noreply.github.com>
2019-03-20 21:51:57 -0700

    Migration for previous change (#322)

--- Data/Migrations/PuzzleServerContextModelSnapshot.cs
+++ Data/Migrations/PuzzleServerContextModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace Data.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.1.4-rtm-31024")
+                .HasAnnotation("ProductVersion", "2.1.8-servicing-32085")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
@@ -484,6 +484,8 @@ namespace Data.Migrations
                     b.Property<string>("Group");
 
                     b.Property<int>("HintCoinsForSolve");
+
+                    b.Property<bool>("HintsAreCumulative");
 
                     b.Property<bool>("IsCheatCode");
 
