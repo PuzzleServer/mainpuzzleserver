b6a7dad1fad018bf04e42f08bd2b4c11b2fafc5a
Lambert Wang <law@microsoft.com>
2019-03-27 19:41:57 -0700

    Show feedback user email to author

--- Data/DataModel/Feedback.cs
+++ Data/DataModel/Feedback.cs
@@ -9,15 +9,54 @@ namespace ServerCore.DataModel
 {
     public class Feedback
     {
+        public Feedback()
+        {
+            Fun = AvgRating;
+            Difficulty = AvgRating;
+        }
+
+        public const int MinRating = 1;
+        public const int AvgRating = 1;
+        public const int MaxRating = 10;
+
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
+
         [Required]
         public virtual Puzzle Puzzle { get; set; }
+
+        /// <summary>
+        /// The user who submitted the feedback>
+        /// </summary>
         [Required]
         public virtual PuzzleUser Submitter {get; set;}
+
+        /// <summary>
+        /// The time the feedback was submitted.
+        /// </summary>
         public DateTime SubmissionTime { get; set; }
+
+        /// <summary>
+        /// True if the puzzle author should not be able to see details about
+        /// the user.
+        /// </summary>
+        public bool IsAnonymous { get; set; }
+
+        /// <summary>
+        /// The feedback text submitted by the user.
+        /// </summary>
         public string WrittenFeedback { get; set; }
+
+        /// <summary>
+        /// The user submitted difficulty score for the puzzle.
+        /// </summary>
+        [Range(MinRating, MaxRating)]
         public int Difficulty { get; set; }
+
+        /// <summary>
+        /// The user submitted fun score for the puzzle.
+        /// </summary>
+        [Range(MinRating, MaxRating)]
         public int Fun { get; set; }
     }
 }
