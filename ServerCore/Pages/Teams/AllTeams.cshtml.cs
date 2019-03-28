72eafb5b65ef35404db6da2815c37adbbc99662d
Jenna Netland <jenetlan@microsoft.com>
2019-03-27 21:37:11 -0700

    Adding an 'All teams' page

---
+++ ServerCore/Pages/Teams/AllTeams.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ServerCore.DataModel;
+
+namespace ServerCore.Pages.Teams
+{
+    /// <summary>
+    /// Player-facing list of all teams
+    /// </summary>
+    [AllowAnonymous]
+    public class AllTeamsModel : TeamListBase
+    {
+        public bool PlayerNotOnTeam { get; set; }
+
+        public AllTeamsModel(PuzzleServerContext serverContext, UserManager<IdentityUser> userManager) : base(serverContext, userManager)
+        {
+        }
+        
+        public async Task<IActionResult> OnGetAsync()
+        {
+            if (LoggedInUser == null)
+            {
+                return Challenge();
+            }
+
+            PlayerNotOnTeam = -1 == await GetTeamId();
+
+            await LoadTeamDataAsync();
+
+            return Page();
+        }
+    }
+}
