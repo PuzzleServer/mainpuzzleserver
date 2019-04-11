82474febb14a60f2131aefd579336faf3ae90526
Morgan Brown <atrader42@hotmail.com>
2019-04-10 22:11:28 -0700

    Add Backstage puzzle forwarder that includes solve times

---
+++ ServerCore/Pages/EventSpecific/PH20/OpeningNight.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using ServerCore.DataModel;
+using ServerCore.Helpers;
+using ServerCore.ModelBases;
+
+namespace ServerCore.Pages.EventSpecific.PH20
+{
+    class BackstageSolveStatus
+    {
+        public string PuzzleName { get; set; }
+        public int SolveTime { get; set; }
+    }
+
+    [Authorize(Policy = "PlayerCanSeePuzzle")]
+    public class OpeningNightModel : EventSpecificPageModel
+    {
+        public OpeningNightModel(PuzzleServerContext serverContext, UserManager<IdentityUser> manager) : base(serverContext, manager)
+        {
+        }
+
+        public async Task<IActionResult> OnGet(int puzzleId)
+        {
+            Puzzle puzzle = await(from puzz in _context.Puzzles
+                                  where puzz.ID == puzzleId
+                                  select puzz).FirstOrDefaultAsync();
+            if (puzzle == null)
+            {
+                return NotFound();
+            }
+
+            // Restrict to the Backstage metapuzzle
+            if (puzzle.Group != "Backstage")
+            {
+                return NotFound();
+            }
+            if (puzzle.SolveValue != 50)
+            {
+                return NotFound();
+            }
+
+            Team team = await UserEventHelper.GetTeamForPlayer(_context, Event, LoggedInUser);
+
+            var puzzleQuery = PuzzleStateHelper.GetFullReadOnlyQuery(_context, Event, null, team, null);
+            var solvedBackstagePuzzles = await (from PuzzleStatePerTeam pspt in puzzleQuery
+                                                where pspt.SolvedTime != null && pspt.UnlockedTime != null && pspt.Puzzle.IsPuzzle && pspt.Puzzle.Group == "Backstage"
+                                                select new BackstageSolveStatus() { PuzzleName = pspt.Puzzle.Name, SolveTime = (int)(pspt.SolvedTime.Value - pspt.UnlockedTime.Value).TotalSeconds }).ToListAsync();
+
+            string json = JsonConvert.SerializeObject(solvedBackstagePuzzles);
+            string escapedJson = Uri.EscapeDataString(json);
+            UriBuilder builder = new UriBuilder("http://wwww.example.com");
+            builder.Query = $"solveData={escapedJson}";
+            return Redirect(builder.ToString());
+        }
+    }
+}
