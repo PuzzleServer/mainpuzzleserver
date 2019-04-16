908164679fa1442cf3a9ee9d8d081d2944541829
Morgan Brown <atrader42@hotmail.com>
2019-04-15 19:02:50 -0700

    Add a view for all feedback

--- ServerCore/Pages/Puzzles/Feedback.cshtml.cs
+++ ServerCore/Pages/Puzzles/Feedback.cshtml.cs
@@ -6,23 +6,33 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ServerCore.DataModel;
+using ServerCore.Helpers;
 using ServerCore.ModelBases;
 using System.ComponentModel.DataAnnotations;
+
+
 namespace ServerCore.Pages.Puzzles
 {
     /// <summary>
     /// Model for the author/admin's view of the feedback items for a specific puzzle
     /// /used for viewing and aggregating feedback for a specific puzzle
     /// </summary>
-    [Authorize(Policy = "IsEventAdminOrAuthorOfPuzzle")]
+    [Authorize(Policy = "IsEventAdminOrEventAuthor")]
     public class FeedbackModel : EventSpecificPageModel
     {
         public FeedbackModel(PuzzleServerContext serverContext, UserManager<IdentityUser> userManager) : base(serverContext, userManager)
         {
         }
 
-        public IList<Feedback> Feedbacks { get; set; }
-        public Puzzle Puzzle { get; set; }
+        public class FeedbackView
+        {
+            public Puzzle Puzzle { get; set; }
+            public Feedback Feedback { get; set; }
+            public PuzzleUser Submitter { get; set; }
+        }
+
+        public IList<FeedbackView> Feedbacks { get; set; }
+        public string PuzzleName { get; set; }
         [DisplayFormat(DataFormatString = "{0:n2}", ApplyFormatInEditMode = true)]
         public double FunScore;
         [DisplayFormat(DataFormatString = "{0:n2}", ApplyFormatInEditMode = true)]
@@ -31,22 +41,58 @@ namespace ServerCore.Pages.Puzzles
         /// <summary>
         /// Gets the feedback and puzzle associated with the given ID
         /// </summary>
-        public async Task<IActionResult> OnGetAsync(int puzzleId)
+        public async Task<IActionResult> OnGetAsync(int? puzzleId)
         {
-            Feedbacks = await _context.Feedback.Where((f) => f.Puzzle.ID == puzzleId).Include("Submitter").ToListAsync();
-            Puzzle = await _context.Puzzles.Where((p) => p.ID == puzzleId).FirstOrDefaultAsync();
-
-            if (Puzzle == null) 
-            { 
-                return NotFound("Could not find puzzle for this feedback page."); 
+            IQueryable<FeedbackView> feedbackRows;
+            if (puzzleId == null)
+            {
+                IQueryable<Puzzle> puzzles;
+                if (EventRole == EventRole.admin)
+                {
+                    puzzles = from puzzle in _context.Puzzles
+                              where puzzle.Event == Event
+                              select puzzle;
+                }
+                else
+                {
+                    puzzles = UserEventHelper.GetPuzzlesForAuthorAndEvent(_context, Event, LoggedInUser);
+                }
+                feedbackRows = from feedback in _context.Feedback
+                               join Puzzle puzzle in puzzles on feedback.Puzzle equals puzzle
+                               join PuzzleUser submitter in _context.PuzzleUsers on feedback.Submitter equals submitter
+                               select new FeedbackView() { Puzzle = puzzle, Feedback = feedback, Submitter = submitter };
             }
+            else
+            {
+                Puzzle puzzle = await (from Puzzle p in _context.Puzzles
+                                       where p.ID == puzzleId.Value && p.Event == Event
+                                       select p).FirstOrDefaultAsync();
+                if (puzzle == null)
+                {
+                    return NotFound();
+                }
+
+                if (EventRole == EventRole.author && !await UserEventHelper.IsAuthorOfPuzzle(_context, puzzle, LoggedInUser))
+                {
+                    return Forbid();
+                }
+
+                PuzzleName = puzzle.Name;
+
+                feedbackRows = from feedback in _context.Feedback
+                               join PuzzleUser submitter in _context.PuzzleUsers on feedback.Submitter equals submitter
+                               where feedback.Puzzle == puzzle
+                               select new FeedbackView() { Puzzle = puzzle, Feedback = feedback, Submitter = submitter };
+            }
+
+            Feedbacks = await feedbackRows.ToListAsync();
             
             FunScore = 0.0;
             DiffScore = 0.0;
             var count = 0;
             foreach (var item in Feedbacks) {
-                FunScore += item.Fun;
-                DiffScore += item.Difficulty;
+                FunScore += item.Feedback.Fun;
+                DiffScore += item.Feedback.Difficulty;
                 count++;
             }
             FunScore /= count;
