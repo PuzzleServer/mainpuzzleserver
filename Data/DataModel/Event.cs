f849629bb23cbc0062996bccdc3ffb49ddab9582
philipzloh <philip.z.loh@gmail.com>
2019-05-28 12:18:21 -0700

    Revert "Revert "Jessica/hide hints (#474)" (#476)"
    This reverts commit 91069efa94711d266677d936337906efb47f50d8.

--- Data/DataModel/Event.cs
+++ Data/DataModel/Event.cs
@@ -161,5 +161,10 @@ namespace ServerCore.DataModel
         /// in email-only mode.
         /// </summary>
         public uint MaxSubmissionCount { get; set; }
+
+        /// <summary>
+        /// True if the hint system will not be used
+        /// </summary>
+        public bool HideHints { get; set; }
     }
 }
