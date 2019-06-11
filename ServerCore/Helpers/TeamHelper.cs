dd2d42df35075c12241ca47803ad23d7364418a5
Jessica Hoang <jessicth@uci.edu>
2019-06-10 20:36:17 -0700

    Stop FTEs from registering for intern events (#484)
    * modified hi.md for Jessica
    
    * Stop FTEs from registering for intern event
    
    * fixed comments
    
    * Made it prettier
    
    * Fixed duplicate code
    
    * fixed comments
    
    * Added parentheses

--- ServerCore/Helpers/TeamHelper.cs
+++ ServerCore/Helpers/TeamHelper.cs
@@ -136,5 +136,14 @@ namespace ServerCore.Helpers
 
             return new Tuple<bool, string>(true, "");
         }
+
+        /// <summary>
+        /// Returns whether or not a user is a microsoft employee that is not an intern (e.g. FTEs and contractors)
+        /// </summary>
+        /// <returns>True if the user a microsoft employee that is not an intern</returns>
+        public static bool IsMicrosoftNonIntern(string email)
+        {
+            return email.EndsWith("@microsoft.com") && !email.StartsWith("t-");
+        }
     }
 }
