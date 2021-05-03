7295dd05503bd14f90e09172ce5ca6f1f29da86a
Morgan Brown <atrader42@hotmail.com>
2021-05-02 23:23:21 -0700

    Add a page for players to see approved freeform submissions that were marked sharable. The page is only available after the event is over

---
+++ ServerCore/Pages/Events/PlayerSubmissions.cshtml.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ServerCore.DataModel;
+using ServerCore.ModelBases;
+
+namespace ServerCore.Pages.Events
+{
+    public class SubmissionView
+    {
+        public string TeamName;
+        public string SubmissionText;
+        public bool Linkify;
+    }
+
+    [Authorize(Policy = "IsRegisteredForEvent")]
+    public class PlayerSubmissionsModel : EventSpecificPageModel
+    {
+        /// <summary>
+        /// True if content isn't available for the user yet
+        /// </summary>
+        public bool NoContent { get; set; }
+
+        /// <summary>
+        /// List of puzzles to choose
+        /// </summary>
+        public List<SelectListItem> FreeformPuzzles { get; set; } = new List<SelectListItem>();
+
+        /// <summary>
+        /// /Submissions for the current puzzle
+        /// </summary>
+        public List<SubmissionView> Submissions { get; set; } = new List<SubmissionView>();
+
+        /// <summary>
+        /// Current puzzle
+        /// </summary>
+        public String PuzzleName { get; set; }
+
+        public PlayerSubmissionsModel(PuzzleServerContext serverContext, UserManager<IdentityUser> userManager) : base(serverContext, userManager)
+        {
+        }
+
+        public async Task<IActionResult> OnGetAsync(int? puzzleId)
+        {
+            // Hide submissions from players until the event is over
+            if (EventRole != EventRole.admin && DateTime.UtcNow <= Event.AnswersAvailableBegin)
+            {
+                NoContent = true;
+                return Page();
+            }
+
+
+            FreeformPuzzles = await (from puzzle in _context.Puzzles
+                                     where puzzle.EventID == Event.ID &&
+                                     puzzle.IsFreeform
+                                     select new SelectListItem()
+                                     {
+                                         Text = puzzle.Name,
+                                         Value = puzzle.ID.ToString(),
+                                         Selected = (puzzleId.HasValue && puzzle.ID == puzzleId)
+                                     }).ToListAsync();
+
+            if (puzzleId == null)
+            {
+                return Page();
+            }
+
+            Puzzle selectedPuzzle = await (from puzzle in _context.Puzzles
+                                           where puzzle.ID == puzzleId &&
+                                           puzzle.EventID == Event.ID &&
+                                           puzzle.IsFreeform
+                                           select puzzle).FirstOrDefaultAsync();
+
+            if (selectedPuzzle == null)
+            {
+                return NotFound();
+            }
+
+            PuzzleName = selectedPuzzle.Name;
+
+            Submissions = await (from submission in _context.Submissions
+                                 join puzzle in _context.Puzzles on submission.PuzzleID equals puzzle.ID
+                                 join team in _context.Teams on submission.TeamID equals team.ID
+                                 where
+                                 puzzle.ID == puzzleId &&
+                                 puzzle.EventID == Event.ID &&
+                                 puzzle.IsFreeform &&
+                                 submission.AllowFreeformSharing &&
+                                 submission.FreeformAccepted.HasValue &&
+                                 submission.FreeformAccepted.Value
+                                 orderby team.Name
+                                 select new SubmissionView()
+                                 {
+                                     SubmissionText = submission.UnformattedSubmissionText,
+                                     TeamName = team.Name,
+                                     Linkify = Uri.IsWellFormedUriString(submission.UnformattedSubmissionText, UriKind.Absolute)
+                                 }
+                                 ).ToListAsync();
+
+            return Page();
+        }
+    }
+}
