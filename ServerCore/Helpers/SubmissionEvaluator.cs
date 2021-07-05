18a89b551bb6bdce2d16ae529e603514d7315154
Megan Quinn <asyasky@gmail.com>
2021-07-04 18:19:47 -0700

    First refactoring pass

---
+++ ServerCore/Helpers/SubmissionEvaluator.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ServerCore.DataModel;
+using ServerCore.ModelBases;
+using static ServerCore.ModelBases.EventSpecificPageModel;
+
+namespace ServerCore.Helpers
+{
+
+    public enum SubmissionResponseCode
+    {
+        Correct,
+        Incorrect,
+        Freeform,
+        Partial,
+        Unauthorized,
+        PuzzleNotFound,
+        PuzzleLocked,
+        EmptySubmission
+    }
+
+    public class SubmissionEvaluator
+    {
+        public PuzzleStatePerTeam PuzzleState { get; set; }
+
+        public string SubmissionText { get; set; }
+
+        public List<SubmissionView> SubmissionViews { get; set; }
+
+        public Puzzle Puzzle { get; set; }
+
+        public Team Team { get; set; }
+
+        public string AnswerToken { get; set; }
+
+        public IList<Puzzle> PuzzlesCausingGlobalLockout { get; set; }
+
+        public bool DuplicateSubmission { get; set; }
+
+        public bool AllowFreeformSharing { get; set; }
+
+        private IList<Submission> Submissions { get; set; }
+
+        private static PuzzleServerContext _context;
+
+        public class SubmissionView
+        {
+            public Submission Submission { get; set; }
+            public Response Response { get; set; }
+            public string SubmitterName { get; set; }
+            public bool IsFreeform { get; set; }
+            public string FreeformReponse { get; set; }
+        }
+
+        public static async Task<SubmissionResponseCode> EvaluateSubmission(PuzzleServerContext context, string submissionText)
+        {
+            if (String.IsNullOrWhiteSpace(submissionText))
+            {
+                return SubmissionResponseCode.EmptySubmission;
+            }
+
+            string SubmissionText = submissionText;
+
+            Team = await UserEventHelper.GetTeamForPlayer(_context, Event, LoggedInUser);
+
+            Puzzle = await _context.Puzzles.Where(
+                (p) => p.ID == puzzleId).FirstOrDefaultAsync();
+
+            if (DateTime.UtcNow < Event.EventBegin)
+            {
+                return SubmissionResponseCode.Unauthorized;
+            }
+
+            PuzzleState = await (PuzzleStateHelper
+      .GetFullReadOnlyQuery(
+          _context,
+          Event,
+          Puzzle,
+          Team))
+      .FirstAsync();
+
+        }
+
+       
+    }
+}
---
+++ ServerCore/Pages/CustomHtmlPageLoader.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ServerCore.DataModel;
+using ServerCore.Helpers;
+
+namespace ServerCore.Pages
+{
+    public class CustomHtmlPageLoader : Controller
+    {
+        private readonly PuzzleServerContext context;
+        private readonly UserManager<IdentityUser> userManager;
+
+        public CustomHtmlPageLoader(PuzzleServerContext context, UserManager<IdentityUser> userManager)
+        {
+            this.context = context;
+            this.userManager = userManager;
+        }
+
+        // GET api/custom
+        [HttpGet]
+        [Route("{eventId}/{puzzleId}/api/custom/")]
+        public async Task<IActionResult> Index(string eventId, int puzzleId)
+        {
+            Event currentEvent = await EventHelper.GetEventFromEventId(context, eventId);
+            if (currentEvent == null)
+            {
+                return Content("ERROR:  That event doesn't exist");
+            }
+
+            PuzzleUser user = await PuzzleUser.GetPuzzleUserForCurrentUser(context, User, userManager);
+            if (user == null)
+            {
+                return Content("ERROR:  You aren't logged in");
+            }
+
+            Team team = await UserEventHelper.GetTeamForPlayer(context, currentEvent, user);
+            if (team == null)
+            {
+                return Content("ERROR:  You're not on a team");
+            }
+
+            Puzzle thisPuzzle = await context.Puzzles.FirstOrDefaultAsync(m => m.ID == puzzleId);
+            if (thisPuzzle == null)
+            {
+                return Content("ERROR:  That's not a valid puzzle ID");
+            }
+
+            if (!currentEvent.AreAnswersAvailableNow)
+            {
+                var puzzleState = await (from state in context.PuzzleStatePerTeam
+                                         where state.Puzzle == thisPuzzle && state.Team == team
+                                         select state).FirstOrDefaultAsync();
+                if (puzzleState == null || puzzleState.UnlockedTime == null)
+                {
+                    return Content("ERROR:  You haven't unlocked this puzzle yet");
+                }
+            }
+
+            // Find the material file with the latest-alphabetically ShortName that contains the substring "customPage".
+            var materialFile = await (from f in context.ContentFiles
+                                      where f.Puzzle == thisPuzzle && f.FileType == ContentFileType.PuzzleMaterial && f.ShortName.Contains("customPage")
+                                      orderby f.ShortName descending
+                                      select f).FirstOrDefaultAsync();
+            if (materialFile == null)
+            {
+                return Content("ERROR:  There's no custom page uploaded for this puzzle");
+            }
+
+            var materialUrl = materialFile.Url;
+
+            // Download that material file.
+            string fileContents;
+            using (var wc = new System.Net.WebClient())
+            {
+                fileContents = await wc.DownloadStringTaskAsync(materialUrl);
+            }
+
+            // Return the file contents to the user.
+            return Content(fileContents, "text/html");
+        }
+
+        // POST api/custom/submit
+        [HttpPost]
+        [Route("{eventId}/{puzzleId}/api/custom/submit/")]
+        public async Task<IActionResult> Post(string eventId, int puzzleId, string submissionText)
+        {
+            // Find what team this user is on, relative to the event.
+
+            Event currentEvent = await EventHelper.GetEventFromEventId(context, eventId);
+            if (currentEvent == null) { return Unauthorized(); }
+            PuzzleUser user = await PuzzleUser.GetPuzzleUserForCurrentUser(context, User, userManager);
+            if (user == null) { return Unauthorized(); }
+            Team team = await UserEventHelper.GetTeamForPlayer(context, currentEvent, user);
+            if (team == null) { return Unauthorized(); }
+
+            var response = PuzzleStateHelper.SubmitAnswer(context, submissionText);
+            return Json(response);
+        }
+    }
+}
