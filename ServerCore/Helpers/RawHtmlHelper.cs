fecc9520a13696ce847517102468ac5b58470509
Kenny Young <tabascq@hotmail.com>
2022-04-19 15:19:44 -0700

    Refine Raw HTML support
    New algorithm that lets the "Html.Raw(" part be prefixed with any text you desire, to support sorting order. Also, moved all the logic into a static method so it wouldn't be strewn all over.

---
+++ ServerCore/Helpers/RawHtmlHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ServerCore.Helpers
+{
+    public static class RawHtmlHelper
+    {
+        /// <summary>
+        /// Helper for extracting raw HTML from a string if present.
+        /// The syntax we look for is: {anything}Html.Raw({raw html})
+        /// </summary>
+        /// <param name="html">html to search for raw HTML in</param>
+        public static IHtmlContent GetRawHtmlIfPresent<T>(string html, IHtmlHelper<T> helper)
+        {
+            if(html != null && html.EndsWith(")") && html.Contains("Html.Raw("))
+            { 
+                int start = html.IndexOf("Html.Raw(") + 9;
+                return helper.Raw(html.Substring(start, html.Length - start - 1));
+            }
+            else
+            {
+                return helper.DisplayFor(m => html);
+            }
+        }
+    }
+}
