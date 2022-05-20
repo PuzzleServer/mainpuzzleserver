b7a2b626c7a31f1da2baaaa4ccbd1889c10d8e55
Megan Quinn <asyasky@gmail.com>
2022-05-19 22:37:19 -0700

    Add migration

---
+++ Data/Migrations/20220520053701_AddEventHasSwagFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class AddEventHasSwagFilter : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "EventHasSwag",
+                table: "Events",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EventHasSwag",
+                table: "Events");
+        }
+    }
+}
