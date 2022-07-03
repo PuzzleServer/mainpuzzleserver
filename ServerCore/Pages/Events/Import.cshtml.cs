e5a0594c7436beb42650a5ad14cec370f058550a
Kenny Young <42425095+tabascq@users.noreply.github.com>
2022-07-03 13:38:06 -0700

    Import admins when events are imported (#704)
    * Import admins when events are imported
    
    We almost always want this, so why not make it easy?
    
    * Improve perf

--- ServerCore/Pages/Events/Import.cshtml.cs
+++ ServerCore/Pages/Events/Import.cshtml.cs
@@ -48,6 +48,7 @@ namespace ServerCore.Pages.Events
                 return Forbid();
             }
 
+            var sourceEventAdmins = await _context.EventAdmins.Where((a) => a.Event.ID == ImportEventID).ToListAsync();
             var sourceEventAuthors = await _context.EventAuthors.Where((a) => a.Event.ID == ImportEventID).ToListAsync();
             var sourcePuzzles = await _context.Puzzles.Where((p) => p.Event.ID == ImportEventID).ToListAsync();
 
@@ -56,14 +57,26 @@ namespace ServerCore.Pages.Events
 
             using (var transaction = _context.Database.BeginTransaction(System.Data.IsolationLevel.Serializable))
             {
+                // Step 0: Make sure all admins exist
+                var destEventAdminIDs = await (from ea in _context.EventAdmins where ea.EventID == Event.ID select ea.AdminID).ToListAsync();
+                foreach (var sourceEventAdmin in sourceEventAdmins)
+                {
+                    if (!destEventAdminIDs.Contains(sourceEventAdmin.AdminID))
+                    {
+                        var destEventAdmin = new EventAdmins();
+                        destEventAdmin.Admin = sourceEventAdmin.Admin;
+                        destEventAdmin.Event = Event;
+                        _context.EventAdmins.Add(destEventAdmin);
+                    }
+                }
+
                 // Step 1: Make sure all authors exist
+                var destEventAuthorIDs = await (from ea in _context.EventAuthors where ea.EventID == Event.ID select ea.AuthorID).ToListAsync();
                 foreach (var sourceEventAuthor in sourceEventAuthors)
                 {
-                    var destEventAuthor = await _context.EventAuthors.Where((e) => e.Event == Event && e.Author == sourceEventAuthor.Author).FirstOrDefaultAsync();
-
-                    if (destEventAuthor == null)
+                    if (!destEventAuthorIDs.Contains(sourceEventAuthor.AuthorID))
                     {
-                        destEventAuthor = new EventAuthors(sourceEventAuthor);
+                        var destEventAuthor = new EventAuthors(sourceEventAuthor);
                         destEventAuthor.Event = Event;
                         _context.EventAuthors.Add(destEventAuthor);
                     }
