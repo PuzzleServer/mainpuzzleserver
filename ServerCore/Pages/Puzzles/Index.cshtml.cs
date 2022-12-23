22cc21ffc85d764c0f5bf781641ff7d3d7d6bc0c
Josh Bodner <jbodner09@outlook.com>
2022-12-22 17:01:58 -0800

    Add answer links to admin puzzle page

--- ServerCore/Pages/Puzzles/Index.cshtml.cs
+++ ServerCore/Pages/Puzzles/Index.cshtml.cs
@@ -22,6 +22,7 @@ namespace ServerCore.Pages.Puzzles
         {
             public Puzzle Puzzle { get; set; }
             public ContentFile Content { get; set; }
+            public ContentFile Answer { get; set; }
         }
 
         public List<PuzzleView> Puzzles { get; set; }
@@ -32,15 +33,20 @@ namespace ServerCore.Pages.Puzzles
             Dictionary<int, ContentFile> puzzleFiles = await (from file in _context.ContentFiles
                                                               where file.Event == Event && file.FileType == ContentFileType.Puzzle
                                                               select file).ToDictionaryAsync(file => file.PuzzleID);
+            Dictionary<int, ContentFile> answerFiles = await (from file in _context.ContentFiles
+                                                              where file.Event == Event && file.FileType == ContentFileType.Answer
+                                                              select file).ToDictionaryAsync(file => file.PuzzleID);
 
             Puzzles = new List<PuzzleView>();
             foreach (Puzzle puzzle in puzzles)
             {
                 puzzleFiles.TryGetValue(puzzle.ID, out ContentFile content);
+                answerFiles.TryGetValue(puzzle.ID, out ContentFile answer);
                 Puzzles.Add(new PuzzleView()
                 {
                     Puzzle = puzzle,
-                    Content = content
+                    Content = content,
+                    Answer = answer
                 });
             }
 
