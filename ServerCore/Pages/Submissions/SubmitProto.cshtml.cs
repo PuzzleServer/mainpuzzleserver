da8fcb227438a1379d9f677c03964d921a705a4b
Josh Bodner <30329717+jbodner09@users.noreply.github.com>
2023-02-21 13:36:20 -0800

    Add prototype list of puzzles that links to prototype submission page instead of regular submission page (#772)
    * Add page to experiment with iframe embedding of puzzles directly on the website
    
    * Add page to experiment with linking from puzzle list to embedded puzzles

--- ServerCore/Pages/Submissions/SubmitProto.cshtml.cs
+++ ServerCore/Pages/Submissions/SubmitProto.cshtml.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
@@ -8,16 +5,20 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ServerCore.DataModel;
-using ServerCore.Helpers;
 using ServerCore.ModelBases;
 
 namespace ServerCore.Pages.Submissions
 {
+    [Authorize(Policy = "PlayerCanSeePuzzle")]
     public class SubmitProtoModel : EventSpecificPageModel
     {
         public SubmitProtoModel(PuzzleServerContext serverContext, UserManager<IdentityUser> manager) : base(serverContext, manager)
         {
         }
+
+        public Puzzle Puzzle { get; set; }
+
+        public ContentFile PuzzleFile { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int puzzleId)
         {
@@ -28,7 +29,12 @@ namespace ServerCore.Pages.Submissions
 
         private async Task SetupContext(int puzzleId)
         {
+            Puzzle = await _context.Puzzles.Where(
+                (p) => p.ID == puzzleId).FirstOrDefaultAsync();
 
+            PuzzleFile = await _context.ContentFiles.Where(
+                (f) => (f.Event == Event) && (f.Puzzle.ID == puzzleId) && (f.FileType == ContentFileType.Puzzle)
+                ).FirstOrDefaultAsync();
         }
     }
 }
