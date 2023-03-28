898a52414ebb93fb693b7cea5008d29b0d57e1a0
Morgan Brown <atrader42@hotmail.com>
2023-03-27 19:20:00 -0700

    No-op change

--- Data/DataModel/Hint.cs
+++ Data/DataModel/Hint.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
+// Hint: The PR pipeline is supposed to work
 namespace ServerCore.DataModel
 {
     /// <summary>
