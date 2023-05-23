07351d6c150c18c554e5e9874274346d41babba4
Morgan Brown <atrader42@hotmail.com>
2023-05-22 21:45:17 -0700

    Table changes without migration

--- Data/DataModel/Event.cs
+++ Data/DataModel/Event.cs
@@ -41,12 +41,22 @@ namespace ServerCore.DataModel
         public int MaxExternalsPerTeam { get; set; }
         public bool IsInternEvent { get; set; }
 
-        [Column("EventHasSwag")]
-        public bool HasSwag { get; set; }
         public bool HasIndividualLunch { get; set; }
         public bool HasTShirts { get; set; }
         public bool AllowsRemote { get; set; }
         public bool IsRemote { get; set; }
+
+        /// <summary>
+        /// True if the event has individual swag
+        /// </summary>
+        [Column("EventHasSwag")]
+        public bool HasSwag { get; set; }
+
+        /// <summary>
+        /// True if teams can sign up for collective swag
+        /// </summary>
+        public bool EventHasTeamSwag { get; set; }
+
         public DateTime TeamRegistrationBegin { get; set; }
         public DateTime TeamRegistrationEnd { get; set; }
 
@@ -97,6 +107,11 @@ namespace ServerCore.DataModel
         public DateTime TeamMiscDataChangeEnd { get; set; }
         public DateTime TeamDeleteEnd { get; set; }
         public DateTime EventBegin { get; set; }
+
+        /// <summary>
+        /// Time when lunches can no longer change (whether by users or automatically) so the reports produce a consistent result
+        /// </summary>
+        public DateTime LunchReportDate { get; set; }
 
         /// <summary>
         /// Returns whether or not the event has started. Does not necessarily indicate the event
@@ -225,5 +240,17 @@ namespace ServerCore.DataModel
         /// Publicly-visible term for "group"
         /// </summary>
         public string TermForGroup { get; set; }
+
+        /// <summary>
+        /// How many players share a lunch order, with rounding up
+        /// For example, if this is 3, then 1-3 player teams get 1 lunch order,
+        /// 4-6 player teams get 2 lunch orders etc
+        /// </summary>
+        public int PlayersPerLunch { get; set; }
+
+        /// <summary>
+        /// If no lunch is chosen, what to fill in with
+        /// </summary>
+        public string DefaultLunch { get; set; }
     }
 }
---
+++ Data/DataModel/TeamLunch.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServerCore.DataModel
+{
+    public class TeamLunch
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public int TeamId { get; set; }
+
+        public virtual Team Team { get; set; }
+
+        public string Lunch { get; set; }
+
+        public string LunchModifications { get; set; }
+    }
+}
