397490de8e2803936dc5d6d4dedfa18d21f60763
Morgan Brown <atrader42@hotmail.com>
2023-06-04 19:19:24 -0700

    Fix design time build for ef

---
+++ Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using ServerCore.DataModel;
+
+namespace ServerCore
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<PuzzleServerContext>
+    {
+        public PuzzleServerContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<PuzzleServerContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=PuzzleServer;Trusted_Connection=True;MultipleActiveResultSets=true");
+            optionsBuilder.UseLazyLoadingProxies(true);
+
+            return new PuzzleServerContext(optionsBuilder.Options);
+        }
+    }
+}
