7a6c7dcce14c1f505a06229e937ad03947c62382
Morgan Brown <morganbr@users.noreply.github.com>
2023-06-13 16:14:01 -0700

    Fix EF designer (#797)
    * Fix design time build for ef
    
    * Add comment
    
    ---------
    
    Co-authored-by: Morgan Brown <atrader42@hotmail.com>

---
+++ Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using ServerCore.DataModel;
+
+namespace ServerCore
+{
+    /// <summary>
+    /// Helps EF Core tools create a PuzzleServerContext when the site isn't running, e.g. for scaffolding.
+    /// Never runs at runtime.
+    /// </summary>
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<PuzzleServerContext>
+    {
+        public PuzzleServerContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<PuzzleServerContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=PuzzleServer;Trusted_Connection=True;MultipleActiveResultSets=true");
+            optionsBuilder.UseLazyLoadingProxies(true);
+
+            return new PuzzleServerContext(optionsBuilder.Options);
+        }
+    }
+}
