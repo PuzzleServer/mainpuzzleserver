f302ac5e7fae1e2f2606b30bf0ac097eeb9594ca
Morgan Brown <atrader42@hotmail.com>
2023-06-15 03:44:45 -0700

    Cleanup

--- Data/DataModel/TeamLunch.cs
+++ Data/DataModel/TeamLunch.cs
@@ -2,6 +2,9 @@
 
 namespace ServerCore.DataModel
 {
+    /// <summary>
+    /// One lunch item being ordered by a team. One-to-many relationship
+    /// </summary>
     public class TeamLunch
     {
         public int ID { get; set; }
@@ -9,10 +12,19 @@ namespace ServerCore.DataModel
         [Required]
         public int TeamId { get; set; }
 
+        /// <summary>
+        /// The team whose lunch this is
+        /// </summary>
         public virtual Team Team { get; set; }
 
+        /// <summary>
+        /// Description of the lunch order
+        /// </summary>
         public string Lunch { get; set; }
 
+        /// <summary>
+        /// Modifications like "no cheese"
+        /// </summary>
         public string LunchModifications { get; set; }
     }
 }
