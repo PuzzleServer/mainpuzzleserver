005dd9afdba080a9e2e375076965a278f9cf9726
Megan Quinn <asyasky@gmail.com>
2023-07-02 17:27:59 -0700

    - Add Room class to DataModel

---
+++ Data/DataModel/Room.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace ServerCore.DataModel
+{
+    /// <summary>
+    /// Provides a structure for associating room and teams within an event
+    /// </summary>
+    public class Room
+    {
+        /// <summary>
+        /// The database generated ID
+        /// </summary>
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        /// <summary>
+        /// The ID for the event the room is a part of
+        /// </summary>
+        [ForeignKey("Event.ID")]
+        [Required]
+        public int EventID { get; set; }
+
+        /// <summary>
+        /// The event the room is a part of
+        /// </summary>
+        [Required]
+        public virtual Event Event { get; set; }
+
+        /// <summary>
+        /// The building the room is in (optional - building can be included in room string if preferred)
+        /// </summary>
+        public string Building { get; set; }
+
+        /// <summary>
+        /// The room number (or building + room if preferred)
+        /// </summary>
+        [Required]
+        public string RoomNumber { get; set; }
+
+        /// <summary>
+        /// The ID for the team that is associated with this room
+        /// </summary>
+        [ForeignKey("Team.ID")]
+        public int? TeamID { get; set; }
+
+        /// <summary>
+        /// The team that is associated with this room
+        /// </summary>
+        public virtual Team Team { get; set; }
+
+        /// <summary>
+        /// Determines whether the room is currently available for teams to choose (allows admin to control list without having to add/delete)
+        /// </summary>
+        [Required]
+        [DefaultValue(false)]
+        public bool CurrentlyOnline { get; set; }
+    }
+}
