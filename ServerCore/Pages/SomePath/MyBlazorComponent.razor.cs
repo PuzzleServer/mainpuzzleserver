ad61192c428aeb1ab0d95b8007ac2d535f3d84f6
Morgan Brown <atrader42@hotmail.com>
2023-07-25 20:59:35 -0700

    Example blazor component with codebehind

---
+++ ServerCore/Pages/SomePath/MyBlazorComponent.razor.cs
@@ -0,0 +1,23 @@
+﻿namespace ServerCore.Pages.SomePath
+{
+    /// <summary>
+    /// Codebehind class for a component. Note that it's partial
+    /// </summary>
+    public partial class MyBlazorComponent
+    {
+        /// <summary>
+        /// Property that can be referenced from the layout code
+        /// </summary>
+        public int CounterVal { get; set; } = 0;
+
+        /// <summary>
+        /// Event handler that can be referenced by the layout code
+        /// </summary>
+        void IncrementCounter()
+        {
+            // Modifying the property in the event handler automatically updates the rendered view
+            // If this were called from something other than Blazor, it should call InvokeAsync(() => StateHasChanged());
+            CounterVal++;
+        }
+    }
+}
