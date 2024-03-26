b3436658aae522f45472bc0c22cc3a54031b735e
Morgan Brown <morganbr@users.noreply.github.com>
2024-03-25 20:23:48 -0700

    Presence (#961)
    * Basic backend signalr infrastructure for notifications. Passes all messages through azure signalr. Still contains lots of todos
    
    * Manage Azure SignalR scalability
    
    * Cleanup/refactoring
    
    * Adds a presence component to the answer submission page that shows a live view of which players on your team are on the page. Still needs:
    * Saving the current state so new page loads see current users
    * Filtering messages by team and puzzle (hopefully efficiently)
    * Faster events (probably javascript visiblitychange event) for the user leaving since the blazor circuit takes a long time to go away
    * Maybe an idle concept based on monitoring for javascript page interaction events
    * Better UI
    
    * Fix async events
    
    * Implement storage for presence and fix display. Still needs cross-machine startup syncing
    
    * Sync state on startup and handle async listener initialization
    
    * Address PR comments and fix AllPresence getting sent to itself instead of the requesting instance
    
    ---------
    
    Co-authored-by: Morgan Brown <atrader42@hotmail.com>

---
+++ ServerCore/ServerMessages/ServerMessageHub.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ServerCore.ServerMessages
+{
+    /// <summary>
+    /// Server hub for server messages. Note that the class is necessary even though it doesn't look like it does anything
+    /// </summary>
+    public class ServerMessageHub : Hub
+    {
+    }
+
+    /// <summary>
+    /// Extension methods to put strongly typed wrappers around broadcasting messages through the hub
+    /// </summary>
+    public static class ServerMessageHubExtensions
+    {
+        /// <summary>
+        /// Group to always send messages to.
+        /// Don't send messages to all clients as we don't prevent other clients from connecting, but they can't get added to this group.
+        /// </summary>
+        const string ServersGroup = "servers";
+
+        public static async Task BroadcastPresenceMessageAsync(this IHubContext<ServerMessageHub> hub, PresenceMessage message)
+        {
+            await hub.Clients.Groups(ServersGroup).SendAsync(nameof(PresenceMessage), message);
+        }
+
+        public static async Task BroadcastGetPresenceStateAsync(this IHubContext<ServerMessageHub> hub, string clientId)
+        {
+            await hub.Clients.Groups(ServersGroup).SendAsync(nameof(GetPresenceState), new GetPresenceState { Client = clientId });
+        }
+
+        public static async Task SendAllPresenceState(this IHubContext<ServerMessageHub> hub, string client, PresenceMessage[] allPresence)
+        {
+            await hub.Clients.Clients(client).SendAsync(nameof(AllPresenceState), new AllPresenceState { AllPresence = allPresence });
+        }
+    }
+}
