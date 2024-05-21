d0d46e7e3e97a2618ccb86ed6394317d6ebca266
Josh Bodner <jbodner09@outlook.com>
2024-05-20 19:28:16 -0700

    Add lunch to database

---
+++ Data/Migrations/20240521022715_LunchOptionCustomizable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class LunchOptionCustomizable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LunchOptions",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LunchOptions",
+                table: "Events");
+        }
+    }
+}
