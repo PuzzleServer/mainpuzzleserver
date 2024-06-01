a91e3fbc80beac2c5ca271480d45f1baa3d02c62
Josh Bodner <30329717+jbodner09@users.noreply.github.com>
2024-05-31 18:21:50 -0700

    Submissions box tweak (#1006)
    * submissions box tweak
    
    * Change action text
    
    * infernal whitespace

--- ServerCore/Pages/Submissions/Index.cshtml.cs
+++ ServerCore/Pages/Submissions/Index.cshtml.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.WindowsAzure.Storage.Blob;
 using ServerCore.DataModel;
 using ServerCore.Helpers;
 using ServerCore.ModelBases;
