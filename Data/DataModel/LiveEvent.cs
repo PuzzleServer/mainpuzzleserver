d3b9edc52172ae2431ebc9f89806f88b272bfa8e
Megan Quinn <asyasky@gmail.com>
2024-07-06 17:45:16 -0700

    Add data model

---
+++ Data/DataModel/LiveEvent.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace ServerCore.DataModel
+{
+    public class LiveEvent
+    {
+        /// <summary>
+        /// The ID
+        /// </summary>
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public int AssociatedPuzzleId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public Puzzle AssociatedPuzzle { get; set; }
+
+        public string Location { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime EventStartTime { get; set; }
+
+        public DateTime EventEndTime { get; set; }
+
+        public bool EventIsScheduled { get; set; }
+
+        public TimeSpan TimePerSlot { get; set; }
+
+        public int TeamsPerSlot { get; set; }
+
+        public TimeSpan ClosingReminderOffset { get; set; }
+
+        public TimeSpan FirstReminderOffset { get; set; }
+
+        public TimeSpan LastReminderOffset { get; set; }
+    }
+}
---
+++ Data/DataModel/LiveEventSchedule.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace ServerCore.DataModel
+{
+    public class LiveEventSchedule
+    {
+        /// <summary>
+        /// The ID
+        /// </summary>
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public int LiveEventId { get; set; }
+
+        [Required]
+        public int TeamId { get; set; }
+
+        [Required]
+        public DateTime StartTime { get; set; }
+
+        public LiveEvent LiveEvent { get; set; }
+    }
+}
