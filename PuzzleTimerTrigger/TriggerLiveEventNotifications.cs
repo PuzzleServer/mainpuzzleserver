779f237c4058cb53a6119bfa09b033551faf4cdf
Morgan Brown <morganbr@users.noreply.github.com>
2024-08-19 20:41:45 -0700

    Azure Function to trigger notifications (#1084)
    * Add project for Azure Functiont that calls the triggernotifications api once a minute to ensure one frontend instance can trigger notifications
    
    * Added hardcoded event id for nipd
    
    * Fixed event id
    
    ---------
    
    Co-authored-by: Morgan Brown <atrader42@hotmail.com>
    Co-authored-by: Jessica Hoang <jehoang@microsoft.com>

---
+++ PuzzleTimerTrigger/TriggerLiveEventNotifications.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace PuzzleTimerTrigger
+{
+    public class TriggerLiveEventNotifications
+    {
+        static HttpClient HttpClient { get; } = new HttpClient();
+        [FunctionName("TriggerLiveEventNotifications")]
+        public void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            const string eventId = "nipd2024";
+            HttpClient.PostAsync($"https://puzzlehunt.azurewebsites.net/api/puzzleapi/liveevent/triggernotifications?eventId={eventId}&timerWindow=10", null).Wait();
+        }
+    }
+}
