c28b902d7fdccb5cfae586f19ec893404208f231
Jessica <jessicth94@gmail.com>
2024-09-09 23:34:40 -0700

    Added migration

---
+++ Data/Migrations/20240910063354_HintCoinsForHelpThread.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class HintCoinsForHelpThread : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CostForHelpThread",
+                table: "Puzzles",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "DefaultCostForHelpThread",
+                table: "Events",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CostForHelpThread",
+                table: "Puzzles");
+
+            migrationBuilder.DropColumn(
+                name: "DefaultCostForHelpThread",
+                table: "Events");
+        }
+    }
+}
