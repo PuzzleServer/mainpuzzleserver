86990d47486051f127943c24ba7a05560b6dadb3
Jessica Hoang <jehoang@microsoft.com>
2024-11-25 23:08:36 -0800

    Remove show help thread

---
+++ Data/Migrations/20241126070829_Remove ShowHelpThreadsToAllGameControl.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveShowHelpThreadsToAllGameControl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShowHelpThreadsToAllGameControl",
+                table: "Events");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ShowHelpThreadsToAllGameControl",
+                table: "Events",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
