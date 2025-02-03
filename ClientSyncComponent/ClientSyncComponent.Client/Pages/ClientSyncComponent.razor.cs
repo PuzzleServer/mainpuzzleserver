801f8889ede051b09dfb1c4d47b7d95c9cfdc02e
Morgan Brown <atrader42@hotmail.com>
2025-02-03 00:18:22 -0800

    Add a client-side blazor component connected to azure tables

---
+++ ClientSyncComponent/ClientSyncComponent.Client/Pages/ClientSyncComponent.razor.cs
@@ -0,0 +1,16 @@
+﻿
+using Azure.Data.Tables;
+
+namespace ClientSyncComponent.Client.Pages
+{
+    public partial class ClientSyncComponent
+    {
+
+        protected override async Task OnInitializedAsync()
+        {
+            TableClient tableClient = new TableClient("UseDevelopmentStorage=true", "WasmTestTable");
+            await tableClient.CreateIfNotExistsAsync();
+            await base.OnInitializedAsync();
+        }
+    }
+}
