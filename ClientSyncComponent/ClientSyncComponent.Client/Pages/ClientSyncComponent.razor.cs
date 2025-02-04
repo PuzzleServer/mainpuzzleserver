f54c76a5dcd9f16f7f8a62d67f46a793d23362df
Morgan Brown <atrader42@hotmail.com>
2025-02-04 01:32:58 -0800

    Add schema for sync table rows

--- ClientSyncComponent/ClientSyncComponent.Client/Pages/ClientSyncComponent.razor.cs
+++ ClientSyncComponent/ClientSyncComponent.Client/Pages/ClientSyncComponent.razor.cs
@@ -1,16 +1,34 @@
 ﻿
 using Azure.Data.Tables;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace ClientSyncComponent.Client.Pages
 {
     public partial class ClientSyncComponent
     {
+        DateTime LastSyncUtc = DateTime.MinValue;
+        TableClient TableClient { get; set; }
 
-        protected override async Task OnInitializedAsync()
+        protected override Task OnParametersSetAsync()
         {
-            TableClient tableClient = new TableClient("UseDevelopmentStorage=true", "WasmTestTable");
-            await tableClient.CreateIfNotExistsAsync();
-            await base.OnInitializedAsync();
+            TableClient = new TableClient("UseDevelopmentStorage=true", "WasmTestTable");
+            return base.OnParametersSetAsync();
+        }
+
+        private async Task SyncAsync(MouseEventArgs e)
+        {
+        }
+
+        private async Task PushChangeAsync(MouseEventArgs e)
+        {
+            PuzzleEntry entry = new PuzzleEntry(
+                puzzleId: 1,
+                teamId: 2,
+                playerId: 3,
+                locationKey: "location",
+                propertyKey: "property",
+                value: "value" + Random.Shared.Next());
+            await TableClient.UpsertEntityAsync(entry);
         }
     }
 }
---
+++ ClientSyncComponent/ClientSyncComponent.Client/PuzzleEntry.cs
@@ -0,0 +1,27 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace ClientSyncComponent.Client
+{
+    public class PuzzleEntry : ITableEntity
+    {
+        public PuzzleEntry(int puzzleId, int teamId, int playerId, string locationKey, string propertyKey, string value)
+        {
+            PartitionKey = $"{puzzleId}_{teamId}";
+            RowKey = $"{locationKey}_{propertyKey}";
+            LocationKey = locationKey;
+            PropertyKey = propertyKey;
+            Value = value;
+        }
+
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public string LocationKey { get; set; }
+        public string PropertyKey { get; set; }
+        public string Value { get; private set; }
+
+        // Set automatically by the Table service
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+    }
+}
