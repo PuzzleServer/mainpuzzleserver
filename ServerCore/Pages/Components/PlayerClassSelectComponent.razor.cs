a04e1da2a403e4d902e3e057fedf1e31b716a24b
Megan Quinn <asyasky@gmail.com>
2025-05-29 13:36:02 -0700

    Default temp player class dropdown to player's main class

--- ServerCore/Pages/Components/PlayerClassSelectComponent.razor.cs
+++ ServerCore/Pages/Components/PlayerClassSelectComponent.razor.cs
@@ -56,6 +56,10 @@ namespace ServerCore.Pages.Components
                 {
                     SelectedPlayerClassID = CurrentTeamMember.TemporaryClass.ID;
                 }
+                else
+                {
+                    SelectedPlayerClassID = CurrentTeamMember.Class.ID;
+                }
 
                 AvailablePlayerClasses = AllPlayerClasses;
             }
