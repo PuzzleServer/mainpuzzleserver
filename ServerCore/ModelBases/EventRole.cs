21e0415f70dd41add2c479c843c61c7bbcd392ee
Kenny Young <tabascq@hotmail.com>
2025-11-26 14:54:27 -0800

    Impersonation
    Impersonation is a means for an author/admin to see part of an event (in a readonly manner) through another team's eyes.
    
    - Allows admins to impersonate teams.
    - Allows authors to impersonate teams within their puzzles (and only their puzzles).
    - Adds several strategic entry points into these impersonation scenarios.
    - Adds authentication for impersonation scenarios.
    - Does not allow impersonators to submit answers.
    - Does not allow impersonators to modify puzzles via ClientSyncComponent.

--- ServerCore/ModelBases/EventRole.cs
+++ ServerCore/ModelBases/EventRole.cs
@@ -1,14 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace ServerCore.ModelBases
 {
-    public enum EventRole
+    public class EventRole
+    {
+        public EventRoleType Type { get; private set; }
+        public int? ImpersonatingTeamId { get; private set; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is EventRole)) return false;
+            EventRole objRole = obj as EventRole;
+            return this.Type == objRole.Type && this.ImpersonatingTeamId == objRole.ImpersonatingTeamId;
+        }
+
+        public static bool operator ==(EventRole left, EventRole right)
+        {
+            if (Object.ReferenceEquals(left, null)) return Object.ReferenceEquals(right, null);
+            return !Object.ReferenceEquals(right, null) && left.Type == right.Type && left.ImpersonatingTeamId == right.ImpersonatingTeamId;
+        }
+
+        public static bool operator !=(EventRole left, EventRole right)
+        {
+            if (Object.ReferenceEquals(left, null)) return !Object.ReferenceEquals(right, null);
+            return Object.ReferenceEquals(right, null) || left.Type != right.Type || left.ImpersonatingTeamId != right.ImpersonatingTeamId;
+        }
+
+        public override string ToString()
+        {
+            return (Type == EventRoleType.impersonate) ? $"{Type}-{ImpersonatingTeamId}" : Type.ToString();
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public static EventRole Parse(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return null;
+            s = s.ToLower();
+
+            if (s.StartsWith("impersonate-"))
+            {
+                bool parse = int.TryParse(s.Substring("impersonate-".Length), out int id);
+                if (!parse)
+                {
+                    return new EventRole() { Type = EventRoleType.play };
+                }
+                return new EventRole() { Type = EventRoleType.impersonate, ImpersonatingTeamId = id };
+            }
+            else
+            {
+                EventRoleType type = Enum.IsDefined(typeof(EventRoleType), s) ? Enum.Parse<EventRoleType>(s): EventRoleType.play;
+                return new EventRole() { Type = type };
+            }
+        }
+
+        public static EventRole admin = new EventRole() { Type = EventRoleType.admin };
+        public static EventRole author = new EventRole() { Type = EventRoleType.author };
+        public static EventRole play = new EventRole() { Type = EventRoleType.play };
+    }
+
+    public enum EventRoleType
     {
         admin = 1,
         author,
-        play
+        play,
+        impersonate
     }
 }
